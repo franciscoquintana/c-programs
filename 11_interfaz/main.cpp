c2440ae816c522ec22972c07bbe226f4c7723d49
franciscoquintana <francisco.quintana.dosa@gmail.com>
2018-02-12 23:11:39 +0100

    Update matriz

--- 11_interfaz/main.cpp
+++ 11_interfaz/main.cpp
@@ -3,18 +3,6 @@
 #include <stdio.h>
 #include "matriz.h"
 
-#define max 100
-int Af, Comun, Bc;
-
-void ver(int A, int C,double m[max][max]){
-    for (int f=0; f<A; f++){
-        for (int c=0; c<C; c++)
-            printf("%6.2lf", m[f][c]);
-        printf("\n");
-    }
-    printf("\n");
-}
-
 void ver(matriz matriz){
     for (int f=0; f<matriz.fila; f++){
         for (int c=0; c<matriz.columna; c++)
@@ -22,28 +10,6 @@ void ver(matriz matriz){
         printf("\n");
     }
     printf("\n");
-}
-
-void pregunta(){
-    printf("Nº Columas matriz 1/ Nº Filas matriz 2: ");
-    scanf(" %i",&Comun);
-    printf("Nº filas matriz 1: ");
-    scanf(" %i",&Af);
-    printf("Nº Columnas matriz 2: ");
-    scanf(" %i",&Bc);
-    printf("\n");
-}
-
-void preguntaDatos(int F, int C,double a[max][max]) {
-    for (int f=0; f<F; f++) {
-        printf("Introduce la fila %i\n",f+1);
-        for (int c=0; c<C; c++) {
-            printf("Introduce la columna %i: ",c+1);
-            scanf("%lf", &a[f][c]);
-        }
-        printf("\n");
-    }
-    ver(F,C,a);
 }
 
 void preguntaMatriz(matriz *matriz) {
@@ -91,29 +57,32 @@ void preguntaMatriz(matriz *matriz) {
 }
 
 int main(){
-    double a[max][max], b[max][max], r[max][max];
 
-    matriz matriz1;
-    
+    matriz matriz1, matriz2, resultado;
+    int comun;
+
     preguntaMatriz(&matriz1);
-
     ver(matriz1);
+    preguntaMatriz(&matriz2);
+    ver(matriz2);
 
-    pregunta();
+    if(matriz1.columna == matriz2.fila)
+        comun = matriz1.columna;
+    else {
+        printf("Error el tamaño de las matrices no coincide, el numero de filas de la matriz 1 y las columnas de la matriz 2 no es el mismo");
+        return 1;
+    }
 
-    printf("Matriz 1:\n");
-    preguntaDatos(Af,Comun,a);
-
-    printf("Matriz 2:\n");
-    preguntaDatos(Comun,Bc,b);
-
-    for (int i=0; i<Af; i++)
-        for(int j=0; j<Bc; j++) {
-            r[i][j] = 0;
-            for (int k=0; k<Comun; k++)
-                r[i][j] += a[i][k] * b[k][j];
+    for (int i=0; i<matriz1.fila; i++)
+        for(int j=0; j<matriz2.columna; j++) {
+            resultado.m[i][j] = 0;
+            for (int k=0; k<comun; k++)
+                resultado.m[i][j] += matriz1.m[i][k] * matriz2.m[k][j];
         }
-    ver(Af,Bc,r);
+
+    resultado.columna = matriz2.columna;
+    resultado.fila = matriz1.fila;
+    ver(resultado);
 
     return EXIT_SUCCESS;
 }
