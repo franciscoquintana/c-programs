5f3b364382f4da5662b8f6e8f1d76502974d8009
franciscoquintana <francisco.quintana.dosa@gmail.com>
2018-11-22 15:12:18 +0100

    Examen

--- examen/common.cpp
+++ examen/common.cpp
@@ -10,7 +10,7 @@ void enviaStr(int server_fd, char *str) {
 char* leeStr(int server_fd) {
     int len;
     read(server_fd, &len, sizeof(int));
-    int size = len * sizeof(char);
+    int size = (len + 1) * sizeof(char);
     char *str = (char *) malloc(size);
     read(server_fd, str, size);
     return str;
